ec0d7377fccd5a815091f3897f0bdd2a6474f125
Abhi Agarwal <me@abhiagarwal.com>
2015-09-11 16:30:53 -0400

    Abstracting a little

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import requests, json
 
+requests.packages.urllib3.disable_warnings()
+
 BASE_URL = 'https://api.axxet.io/'
 
 headers = {'content-type': 'application/json'}
@@ -9,14 +11,18 @@ data = {"username": "axxet", "password": "123123"}
 response = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
 json_response = json.loads(response.text)
 token = json_response['token']
+headers = {'Authorization': 'Bearer ' + token}
 
-url = BASE_URL + 'api/assets/'
-headers = {'Authorization': 'JWT ' + token}
-response = requests.get(url, headers=headers, verify=False)
-json_response = json.loads(response.text)
-print json_response
+def axxet_get(url):
+  response = requests.get(url, headers=headers, verify=False)
+  json_response = json.loads(response.text)
+  return json_response
 
-headers = {'Authorization': 'JWT ' + token}
-response = requests.options(url, headers=headers, verify=False)
-json_response = json.loads(response.text)
-print json_response
+def axxet_options(url):
+  response = requests.options(url, headers=headers, verify=False)
+  json_response = json.loads(response.text)
+  return json_response
+
+print axxet_get(BASE_URL + 'api/assets/')
+# print axxet_options(BASE_URL + 'api/assets/')
+# print axxet_get(BASE_URL + 'api/users/me/')
