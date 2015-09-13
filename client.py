d88f7ff3fcd71a28214d60ecfdfcca07f8264224
Abhi Agarwal <me@abhiagarwal.com>
2015-09-13 10:25:57 -0400

    Cleaning up a little more

--- client.py
+++ client.py
@@ -4,17 +4,29 @@ requests.packages.urllib3.disable_warnings()
 
 BASE_URL = 'https://api.axxet.io/'
 
-headers = {'content-type': 'application/json'}
-url = BASE_URL + 'api-key/get/'
-data = {"username": "axxet", "password": "123123"}
+def get_token(data):
+    headers = {
+        'content-type': 'application/json',
+    }
+    url = BASE_URL + 'api-key/get/'
+    response = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
+    json_response = json.loads(response.text)
+    token = json_response['token']
+    return token
 
-response = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
-json_response = json.loads(response.text)
-token = json_response['token']
-headers = {'Authorization': 'Bearer ' + token}
+token = get_token({"username": "axxet", "password": "123123"})
+headers = {
+    'content-type': 'application/json', 
+    'Authorization': 'Bearer ' + token,
+}
 
 def axxet_get(url):
   response = requests.get(url, headers=headers, verify=False)
+  json_response = json.loads(response.text)
+  return json_response
+
+def axxet_post(url, data):
+  response = requests.post(url, data=json.dumps(data), headers=headers, verify=False)
   json_response = json.loads(response.text)
   return json_response
 
@@ -23,6 +35,10 @@ def axxet_options(url):
   json_response = json.loads(response.text)
   return json_response
 
-print axxet_get(BASE_URL + 'api/assets/')
+# print axxet_get(BASE_URL + 'api/assets/')
 # print axxet_options(BASE_URL + 'api/assets/')
 # print axxet_get(BASE_URL + 'api/users/me/')
+
+###### Adding new Asset
+# axxet_data = {'name': 'fish'}
+# print axxet_post(BASE_URL + 'api/assets/', axxet_data)
